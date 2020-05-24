0f3c557b37b3a64120a5a22d12e054343419ac58
CJHarms <CJHarms@users.noreply.github.com>
2020-05-24 08:29:25 +0200

    Unified Hello for Business Script
    Edited Hello for Business Script to same Script Base as the LSASS one

--- Prevent-HelloforBusiness-After-1st-Login.ps1
+++ Prevent-HelloforBusiness-After-1st-Login.ps1
@@ -1,9 +1,78 @@
-$path = 'HKLM:\SOFTWARE\Policies\Microsoft\PassportForWork'
-$key = try {
-    Get-Item -Path $path -ErrorAction Stop
-}
-catch {
-    New-Item -Path $path -Force
+# Disable forced Windows Hello for Business Auto Provisioning after 1st User Logon. The delayed Provisioning Setting from the GPO is not (yet) available as CSP
+# https://getadmx.com/?Category=Windows_10_2016&Policy=Microsoft.Policies.MicrosoftPassportForWork::MSPassport_UsePassportForWork
+#
+# Original Script by Aaron Parker: https://github.com/aaronparker
+
+Function Set-RegistryValue {
+    <#
+        .SYNOPSIS
+            Creates a registry value in a target key. Creates the target key if it does not exist.
+    #>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $True)]
+        [string] $Key,
+
+        [Parameter(Mandatory = $True)]
+        [string] $Value,
+
+        [Parameter(Mandatory = $True)]
+        $Data,
+
+        [Parameter(Mandatory = $False)]
+        [ValidateSet('Binary', 'ExpandString', 'String', 'Dword', 'MultiString', 'QWord')]
+        [string] $Type = "String"
+    )
+
+    try {
+        If (Test-Path -Path $Key -ErrorAction SilentlyContinue) {
+            Write-Verbose "Path exists: $Key"
+        }
+        Else {
+            Write-Verbose -Message "Does not exist: $Key."
+
+            $folders = $Key -split "\\"
+            $parent = $folders[0]
+            Write-Verbose -Message "Parent is: $parent."
+
+            ForEach ($folder in ($folders | Where-Object { $_ -notlike "*:"})) {
+                New-Item -Path $parent -Name $folder -ErrorAction SilentlyContinue | Out-Null
+                $parent = "$parent\$folder"
+                If (Test-Path -Path $parent -ErrorAction SilentlyContinue) {
+                    Write-Verbose -Message "Created $parent."
+                }
+            }
+            Test-Path -Path $Key -ErrorAction SilentlyContinue
+        }
+    }
+    catch {
+        Write-Error "Failed to create key $Key."
+        Break
+    }
+    finally {
+        Write-Verbose -Message "Setting $Value in $Key."
+        New-ItemProperty -Path $Key -Name $Value -Value $Data -PropertyType $Type -Force -ErrorAction SilentlyContinue | Out-Null
+    }
+
+    $val = Get-Item -Path $Key
+    If ($val.Property -contains $Value) {
+        Write-Verbose "Write value success: $Value"
+        Write-Output $True
+    } Else {
+        Write-Verbose "Write value failed."
+        Write-Output $False
+    }
 }
 
-New-ItemProperty -Path $key.PSPath -Name DisablePostLogonProvisioning -PropertyType DWORD -Value 1
+
+#region Actions
+$stampDate = Get-Date
+$scriptName = ([System.IO.Path]::GetFileNameWithoutExtension($(Split-Path $script:MyInvocation.MyCommand.Path -Leaf)))
+$logFile = "$env:ProgramData\Intune-PowerShell-Logs\$scriptName-" + $stampDate.ToFileTimeUtc() + ".log"
+Start-Transcript -Path $logFile
+
+$key = "HKLM:\SOFTWARE\Policies\Microsoft\PassportForWork"
+Set-RegistryValue -Key $key -Value "DisablePostLogonProvisioning" -Data 1 -Type Dword -Verbose
+
+Stop-Transcript
+#endregion
